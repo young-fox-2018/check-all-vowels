d3ed5440f7dcd7fad9915a86c17288eea456302e
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-10-30 22:26:19 +0700

    check all Vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkVowel (str) {
+    const vocals = ['a','i','u','e','o']
+    
+    let counter = 0
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vocals.length; j++) {
+            if (str[i] === vocals[j]) counter++
+        }
+    }
+    if (counter === str.length) return true
+    else return false
+}
 
+console.log(checkVowel('oioioi'))
+console.log(checkVowel('kata'))
