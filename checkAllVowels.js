7ff06cacc45ee9ffccacd973ef5ed60274da1ded
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-10-30 11:21:13 +0700

    check vowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
 
+function checkVowel(str){
+
+  var result = ''
+
+  for(var i = 0 ; i < str.length ; i++){
+    if(str[i] !== 'a' && str[i] !== 'e' && str[i] !== 'i' && str[i] !== 'o' && str[i] !== 'u'){
+      result+= str[i]
+    }
+  }
+
+  if(result.length !== 0){
+    return false
+  }else{
+    return true
+  }
+}
+
+console.log(checkVowel('aeuiio')) ;
+console.log(checkVowel('aepio'))
