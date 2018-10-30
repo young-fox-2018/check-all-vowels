045e67bf09a8b960deddd47b1faf576c00830249
ranggakusuma <ranggavskusuma@gmail.com>
2018-10-30 11:12:06 +0700

    check all vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(str){
+    var vowels = ['a','i','u','e','o']
+    var finded = false
+    for(i=0;i<str.length;i++){
+        finded = false
+        for(j=0;j<vowels.length;j++){
+            if(str[i].toLowerCase() === vowels[j]){
+                finded = true
+            }
+        }
 
+        if(!finded){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(checkAllVowels('AAIIxUUEEOO')) // FALSE
+ 
