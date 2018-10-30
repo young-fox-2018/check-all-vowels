06f94289a21b067158df6d588a6606168e972995
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-10-30 11:23:32 +0700

    submitting this ex

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,31 @@
+function checkVowel(str){
+    var vowels = "aiueo";
+    var result 
+    var cont = [];
+    
+    for(var i = 0; i < str.length; i++){
+        var check = false;
+        for(var j = 0; j < vowels.length; j++){
+            if(str[i] == vowels[j]){
+                check = true;
+            }
+        }
+        if (check == false){
+            cont.push(str[i])
+        }
+    }
+    if(cont.length === 0){
+        return true
+    }else{
+        return false
+    }
 
+    
+    
+}
+
+console.log(checkVowel("app"))
+console.log(checkVowel("pp"))
+console.log(checkVowel("ai"))
+console.log(checkVowel("aiiiii"))
+console.log(checkVowel("apppa"))
