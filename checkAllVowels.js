3eb30e06bf91f3ec692dd09a4a91a0c710c84967
anharfathoni <anharaf28@gmail.com>
2018-10-30 12:47:04 +0700

    add checkVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,27 @@
+function checkVowel(str){
+    var vokal = "aeiouAIUEO"
+    var isVocal = true
 
+    for(var i=0; i<str.length; i++){
+        for(var j=0; j<vokal.length; j++){
+            if(str[i] === vokal[j]){
+                isVocal = true
+                break
+            }
+            else{
+                isVocal = false
+            }
+        }
+        if(isVocal === false){
+            return isVocal
+        }
+    }
+    return isVocal
+}
+
+console.log(checkVowel("aaaaaa"))
+console.log(checkVowel("aaaaaaiiiuuueooo"))
+
+console.log(checkVowel("aaaaiiuuoob"))
+console.log(checkVowel("baaiiiauuua"))
+console.log(checkVowel("aaiibooaaee"))
