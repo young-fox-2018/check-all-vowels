c6a527fdfd377482f54dadc1f5befdec80623d29
Cascade <mufhabu@gmail.com>
2018-10-30 14:32:40 +0700

    done checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+let wordsLib = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 
+function checkVowels(str){
+    for (let k = 0; k < str.length; k++) {
+        for(let i = 0; i < wordsLib.length; i++){
+            if(str[k] === wordsLib[i]){
+                return 'not all vowel'
+            }
+        }
+    }
+    return 'all vowel'
+}
+
+console.log(checkVowels('apadehh'))
+console.log(checkVowels('auouououoooo'))
+console.log(checkVowels('ea ea ea'))
+console.log(checkVowels('blegedel duel'))
