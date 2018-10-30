26e78e774860d9ea11d8921cfb375fca3518ee64
sendyakbar <msendyakbars@gmail.com>
2018-10-30 11:02:38 +0700

    check all vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkAllVowels(str) {
+    let consonant = 'bcdfghjklmnpqrstvwxyz'
+    let temp = 0
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < consonant.length; j++) {
+            if (str[i] === consonant[j]) {
+                temp++
+                if (temp > 0) {
+                    return false
+                }
+            }            
+        }
+    }
+    return true
+}
 
+// Test Cases
+console.log(checkAllVowels('uiueo')) // true
+console.log(checkAllVowels('ajagijig')) // false
