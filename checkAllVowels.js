d57a234f2a773d92e6f2b3bcaad6002fdda5068c
Januarsmad <januarsmad@gmail.com>
2018-10-30 18:22:24 +0700

    program done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
-
+function checkVowel(input){
+    var cond = true
+    var i = 0
+    while(cond && i < input.length){
+        if(input[i] !== 'a' && input[i] !== 'e' && input[i] !== 'i' && input[i] !== 'o' && input[i] !== 'u'){
+            cond = false
+        }
+        i++
+    }
+    return cond  
+}
+console.log(checkVowel('saya'))
+console.log(checkVowel('aa'))
+console.log(checkVowel('aiueo'))
