e6adaa3d3fcbab95b800447ae98ee14e3d9e3b86
hacktiv8 <arifinrobert2013@gmail.com>
2018-10-30 18:09:06 +0700

    finish the task

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function checkAll(words) {
+    var lowerCasewords = words.toLowerCase();
+    for (var i = 0; i < lowerCasewords.length;i++) {
+        if (lowerCasewords.charCodeAt(i) != 97 && lowerCasewords.charCodeAt(i) != 101 && lowerCasewords.charCodeAt(i) != 105 && lowerCasewords.charCodeAt(i) != 111 && lowerCasewords.charCodeAt(i) != 117) {
+            result = false;
+            break;
+        } else {
+            result = true;
+        }
+    }
+    
+    return result;
+}
 
+console.log(checkAll("AEIOU"));
+console.log(checkAll("aeiou"));
+console.log(checkAll("AeioU"));
+console.log(checkAll("STASDADaa"));
