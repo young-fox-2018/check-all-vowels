3cf9f4adf3df96fb3622ffd09a0d7c7fbd991153
aandroomeedaa <andromeda_011190@yahoo.co.id>
2018-10-30 11:34:18 +0700

    aandroomeedaa

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function checkVowel(str){
+  const notvowel = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+  var result = true
+  for(let i=0;i<str.length;i++){
+    for(let j=0;j<notvowel.length;j++){
+      if(str[i]===notvowel[j]){
+        result = false
+        console.log(result)
+        return result
+      }
+    }
+  }
+  console.log(result)
+  return result
+}
 
+
+checkVowel('ajklsi')
