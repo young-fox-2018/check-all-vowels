0837e0ceed37c435571907807cb7f647b78a9c49
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-30 15:44:11 +0700

    add vowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,28 @@
+function checkVowel(param) {
+    var vow = 'aiueo'
+    var check = true
+
+    if (param == "") {
+        return false
+    }
+
+    for (let i = 0; i < param.length; i++) {
+        for (let j = 0; j < vow.length; j++) {
+            if (param[i].toLowerCase() != vow[i].toLowerCase()) {
+                check = false
+            }
+        }
+        
+    }
+
+    if (check == true) {
+        return true
+    }
+    return false
+    
+}
+
+console.log(checkVowel("ai"));
+console.log(checkVowel(""));
+console.log(checkVowel("abcde"));
 
