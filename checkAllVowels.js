49ad962cc89d737efd54c232a28449a117d6d6d4
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-10-30 16:10:38 +0700

    faishal

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(str) {
+    const vowels = ['a', 'i', 'u', 'e', 'o']
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (str[i] === vowels[j]) {
+                count++
+            }
+        }        
+    }
+    if (count === str.length) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
+console.log(checkAllVowels('haihu'))
+console.log(checkAllVowels('aioeu'))
