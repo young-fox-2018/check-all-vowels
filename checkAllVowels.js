3d688d809a6d18c9749ab968b8ba6fe0b8d89562
vinutama <muhammadkhevin@gmail.com>
2018-10-30 10:58:26 +0700

    checkVowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
 
+function checkVowels(str) {
+    const vowels = ['a', 'i', 'u', 'e', 'o']
+    let countVowel = 0;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (vowels[j] === str[i]) {
+                countVowel++;
+            }
+        }
+        if (countVowel === str.length) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(checkVowels('uuiiaaooiiee'))
+console.log(checkVowels('vowels'))
