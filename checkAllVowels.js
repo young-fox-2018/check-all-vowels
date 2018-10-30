c4e7cf9093e31af1ed6a0bdd2699b549151863a1
Ribka Haryuni <ribka.blue@gmail.com>
2018-10-30 11:46:48 +0700

    check all vocal done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function allVocal(str){
+    const dictionary = ['a', 'i', 'u', 'e', 'o']
 
+    let counter = 0
+
+    for(let i = 0; i < str.length; i++){
+        for(let j = 0; j < dictionary.length; j++){
+            if(str[i] === dictionary[j]){
+                counter ++
+            }
+        }
+    }
+
+    if(counter === str.length) return true
+    else return false
+}
+
+console.log(allVocal("aabaa"))
+console.log(allVocal("aeaaiuao"))
