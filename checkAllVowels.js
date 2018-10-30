fd4f01f3d27a61af58c65ccb62d86d04cd0a8ff6
fauzan_bintang <obin.aji@gmail.com>
2018-10-30 18:49:10 +0700

    check-all-vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
-
+function checkAllVowels(str) {
+    var vokal = ['a', 'i', 'u', 'e', 'o'];
+    for (let i = 0; i < str.length; i++) {
+        var check = false;
+        for (let j = 0; j < vokal.length; j++) {
+            if (str[i] === vokal[j]) {
+                check = true;
+                break;
+            }
+        }
+        if (check === false) {
+            return false
+        }
+    }
+    if (check === true) {
+        return true
+    }
+}
+console.log(checkAllVowels('aio'));
+console.log(checkAllVowels('aioz'));
+console.log(checkAllVowels('aizo'));
