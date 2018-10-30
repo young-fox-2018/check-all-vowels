eee07c3d8e55312a4ee6f4505940e8027187fce8
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-10-30 11:03:50 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+function checkAllVowels(str) {
+    let vocals = ["a", "i", "u", "e", "o"]
+    var isVocal = 0
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vocals.length; j++) {
+            if (str[i] === vocals[j]) {
+                isVocal += 1
+            }
+        }
+    }
+    if (isVocal === str.length) return true
+    else return false
 
+}
+console.log(checkAllVowels('aiueo'));
+console.log(checkAllVowels('racecar'));
+console.log(checkAllVowels('recacar')); 
