fcdd99e11b766127f1cca4b9697ffa27588bcb03
patriagani <patria.gani@gmail.com>
2018-10-30 15:18:03 +0700

    checkAllVowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkVowels(str) {
+  var notVowels = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ "
+  var notVowelsUpperCase = notVowels.toUpperCase()
+  var check = false
+  for (var i = 0; i < str.length; i++) {
+    if (notVowels.indexOf(str[i]) !== -1) {
+      check = false
+      break
+    }
+    else {
+      check = true
+    }
+  }
+  return check
+}
 
+console.log(checkVowels("ai"));
+console.log(checkVowels(""));
+console.log(checkVowels("abcde"));
