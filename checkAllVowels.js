671a65bd47248d891f5f587c77d4416a948f7ecb
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-10-30 11:32:00 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkVowel(str){
+    let vocals = "aiueo"; let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() == "a" || str[i].toLowerCase() == "i" || str[i].toLowerCase() == "u" || str[i].toLowerCase() == "e" || str[i].toLowerCase() == "o") {
+            count += 1
+        }
+    }
+    if (str.length == count) {
+        return true
+    } else {
+        return false
+    }
+}
 
+
+// testcase
+console.log(checkVowel("aaiiuueeoo"))
+console.log(checkVowel("%6836aaui"))
+console.log(checkVowel("AIUEO"))
+console.log(checkVowel("omg"))
