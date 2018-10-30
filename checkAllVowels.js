d167ac1a47924b0a560dabe6e99b1da14c583c81
maitreya <bluespider529@gmail.com>
2018-10-30 18:26:20 +0700

    done check all vowels challenge

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
 
+function checkAllVowels(input){
+    
+    var str = input.toLowerCase();
+    // var vowels = "aiueoAIUEO"
+    for(var i = 0; i < str.length; i++){
+        // console.log(input[i])
+        if(str[i] === "a" || str[i] === "i" || str[i] === "u" || str[i] === "e" || str[i] === "o"){
+            return true
+        } 
+    }
+    return false
+}
+console.log(checkAllVowels("Hello World"));
+console.log(checkAllVowels("aaaaAAAiiiiiiRRRR"));
+console.log(checkAllVowels("modern misery"));
+console.log(checkAllVowels("modern misery"));
+console.log(checkAllVowels("aurora"));
+console.log(checkAllVowels("kkkkkk"));
