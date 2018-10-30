16047f5899efce328844358bc0da6bafe96bb643
Atras Tudhi <atras.r@gmail.com>
2018-10-30 12:32:29 +0700

    check vowel challange done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowel(str) {
+    let lowerCase = str.toLowerCase()
+    let result = true
 
+    for(let i = 0; i < str.length; i++) {
+        if(lowerCase[i] === 'a' || lowerCase[i] === 'i' || lowerCase[i] === 'u' || lowerCase[i] === 'e' || lowerCase[i] === 'o') {
+            result = true
+        } else {
+            return false
+        }
+    }
+
+    return result
+}
+
+console.log(checkVowel('AaaaA');
