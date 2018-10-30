3b33c3c3595d889d39f7850edfafbcdbd37827fe
abednegolubis <lubisabednego@gmail.com>
2018-10-30 12:55:27 +0700

    submit challenges check vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
+function checkVowels(str) {
+    var string = str.toLowerCase();
+    var result = true
+    for (let i = 0; i < string.length; i++) {
+        if(string[i] !== 'a' && string[i] !== 'i' && string[i] !== 'u' && string[i] !== 'e' && string[i] !== 'o')
+            result =  false
+    }
+    return result
+}
 
+console.log(checkVowels('aaaiiuuueet'))
+console.log(checkVowels('aaaiiuuueee'))
+console.log(checkVowels('ataiiuuueee'))
