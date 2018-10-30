f363b83ee6bac44de340b6506486244d59dfe617
javasusanto <herisusanto.id@gmail.com>
2018-10-30 10:53:08 +0700

    Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+var checkVowel = (str) => {
+    let kamus = ['a', 'i', 'u', 'e', 'o'],
+        check = []
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < kamus.length; j++) {
+            if (str[i].toLowerCase() === kamus[j]) {
+                check.push(kamus[j])
+            }
+        }
+    }
+    if (check.length === str.length) return true
+    return false
 
+}
+console.log(checkVowel('iudfadfseo'))
