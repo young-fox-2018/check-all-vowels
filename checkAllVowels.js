61a0cf5d3e875ae8e520e9e107fddef7a6cd7ae0
Desy Armariena <desy.armariena@gmail.com>
2018-10-30 17:42:24 +0700

    Check vowels only on string

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,25 @@
+function checkVowel(str) {
 
+    var char = ['a', 'e', 'i', 'o', 'u'];
+    var vowels = true;
+
+    var i = 0;
+    while (vowels && i < str.length) {
+
+        var vowelsFound = false;
+        for (var j = 0; j < char.length; j++) {
+            if(char[j] === str[i]) {
+                vowelsFound = true;
+            }
+        }
+
+        if(!vowelsFound) vowels = false;
+        
+        i++;
+    }
+
+    return vowels;
+}
+
+console.log(checkVowel('aeiouaeiou'));
+console.log(checkVowel('opaaeioa'));
