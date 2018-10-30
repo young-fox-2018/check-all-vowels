628f1c3750df843d6d08c2ba94f34066d860712a
Arnold Herod <arnold.herod@gmail.com>
2018-10-30 11:11:55 +0700

    initial submission

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,30 @@
+function checkVowels(str){
+    str = str.split("")
+    var vowels = "aiueo"
+
+    for (var i = 0 ; i < str.length ; i++){
+        for(var iVowels = 0 ; iVowels < vowels.length ; iVowels++){
+            if(str[i] === vowels[iVowels]){
+                str[i] = true
+                break;
+            }
+        }
+    }
+    
+    for(var iStr = 0 ; iStr < str.length ; iStr++){
+        if(str[iStr] !== true){
+            return false
+        }
+        
+        if(iStr === str.length-1){
+            return true
+        }
+    }
+}
+
+console.log(checkVowels("aaaa")); //TRUE
+console.log(checkVowels("bcef")); //FALSE
+console.log(checkVowels("eeef")); //FALSE
+console.log(checkVowels("apa")); //FALSE
+console.log(checkVowels("eee")); //FALSE
 
